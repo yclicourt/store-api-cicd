7e403f20e45c8c25ea06081e919d5a93a23daf54
Yoadev <yclicourt@gmail.com>
2025-03-07 11:36:51 -0500

    rollback product controllers

--- README.md
+++ README.md
@@ -10,21 +10,21 @@ nodejs >= 18.20.2
 
 1. Install dependencies
 ```
-npm install
+bun install
 ```
 
 2. Install prisma,prisma client and generate tables 
 ```
-npm install @prisma/client prisma
-npx prisma generate && npx prisma migrate dev --init
+bun install @prisma/client prisma
+bunx prisma generate && bunx prisma migrate dev --init
 ```
 
 3. Test Code
 ```
-npm run test
+bun run test
 ```
 
 4. Start API
 ```
-npm start or npm run dev
+bun start or bun run dev
 ```
--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -31,20 +31,8 @@ const getItems = async (req: Request, res: Response) => {
 };
 const createItem = async (req: Request, res: Response) => {
   try {
-    const { file, body } = req;
-    const dataRegister: Product = {
-      id: 1,
-      description: "",
-      title: "",
-      price: 2300,
-      categoryId: 1,
-      images: `${file?.filename}`,
-    };
-
-    if (!dataRegister.images) {
-      res.status(400).json({ message: "All fields required" });
-    }
-    const responseItem = await createProduct(dataRegister);
+    const { body } = req;
+    const responseItem = await createProduct(body);
     res.send(responseItem);
   } catch (error) {
     console.log(error);
